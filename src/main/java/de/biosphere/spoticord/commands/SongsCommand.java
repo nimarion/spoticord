06d6e255d8e9b7b7d1f9eb562be096a2ba797d4a
niklas <23435250+Biospheere@users.noreply.github.com>
2020-06-09 10:39:06 +0200

    :art: Splitted top command

--- src/main/java/de/biosphere/spoticord/commands/SongsCommand.java
+++ src/main/java/de/biosphere/spoticord/commands/SongsCommand.java
@@ -1,25 +1,21 @@
 package de.biosphere.spoticord.commands;
 
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-
 import java.util.Map;
 
 import de.biosphere.spoticord.database.model.SpotifyTrack;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 
-public class TopCommand extends Command {
+public class SongsCommand extends Command {
 
-    public TopCommand() {
-        super("top", "View the top 10 tracks in this guild");
+    public SongsCommand() {
+        super("songs", "View the top 10 tracks in this guild", "song", "top");
     }
 
     @Override
     public void execute(String[] args, Message message) {
-
         final EmbedBuilder embedBuilder = getEmbed(message.getGuild(), message.getAuthor());
-
-        if (args.length == 0 || args[0].equalsIgnoreCase("server")) {
+        if(message.getMentionedMembers().isEmpty()){
             final Map<SpotifyTrack, Integer> topMap = getBot().getDatabase().getTopTracks(message.getGuild().getId(),
                     null, 10);
             if (!topMap.isEmpty()) {
@@ -33,15 +29,7 @@ public class TopCommand extends Command {
                         spotifyTrack.trackTitle(), spotifyTrack.id(), spotifyTrack.artists(), topMap.get(spotifyTrack)));
                 count++;
             }
-        } else if (args[0].equalsIgnoreCase("user")) {
-            final Map<String, Integer> topMap = getBot().getDatabase().getTopListeners(message.getGuild().getId(), 10);
-            topMap.forEach((k, v) -> {
-                final Member member = message.getGuild().getMemberById(k);
-                if (member != null) {
-                    embedBuilder.appendDescription(String.format("%s (%s) \n", member.getAsMention(), v));
-                }
-            });
-        } else if (!message.getMentionedMembers().isEmpty()) {
+        } else {
             final Map<SpotifyTrack, Integer> topMap = getBot().getDatabase().getTopTracks(message.getGuild().getId(),
                     message.getMentionedMembers().get(0).getId(), 10);
             embedBuilder.setTitle("Top 10 Spotify Tracks");
@@ -54,10 +42,8 @@ public class TopCommand extends Command {
                         spotifyTrack.trackTitle(), spotifyTrack.id(), spotifyTrack.artists(), topMap.get(spotifyTrack)));
                 count++;
             }
-        } else {
-            embedBuilder.setDescription("+top [user,server,mention]");
         }
-
         message.getChannel().sendMessage(embedBuilder.build()).queue();
     }
+
 }
