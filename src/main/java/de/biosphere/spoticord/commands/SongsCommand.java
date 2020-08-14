f9d1f7b603785a0dcecc63ed75f06788e3c9b1a3
Markus <m.kording@hotmail.de>
2020-08-14 23:04:49 +0200

    Fix little bug and add more information's to the footer

--- src/main/java/de/biosphere/spoticord/commands/SongsCommand.java
+++ src/main/java/de/biosphere/spoticord/commands/SongsCommand.java
@@ -5,21 +5,26 @@ import de.biosphere.spoticord.DiscordUtils;
 import de.biosphere.spoticord.database.model.SpotifyTrack;
 import de.biosphere.spoticord.enums.TrackFilter;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SongsCommand extends Command {
 
     private static final Integer MAX_FETCH_DAYS;
     private static final Integer DEFAULT_DAYS;
     private static final String REGEX;
+    private static final Pattern MENTION_PATTERN;
 
     static {
         MAX_FETCH_DAYS = DiscordUtils.getIntFromString(Configuration.MAX_FETCH_DAYS, 30);
         DEFAULT_DAYS = DiscordUtils.getIntFromString(Configuration.DEFAULT_DAYS, 7);
         REGEX = "^[+]?\\d{1,%s}+$".formatted(MAX_FETCH_DAYS.toString().length());
+        MENTION_PATTERN = Message.MentionType.USER.getPattern();
     }
 
     public SongsCommand() {
@@ -28,7 +33,6 @@ public class SongsCommand extends Command {
 
     @Override
     public void execute(String[] args, Message message) {
-        final EmbedBuilder embedBuilder = getEmbed(message.getGuild(), message.getAuthor());
         final Member member = DiscordUtils.getAddressedMember(message);
         final int lastDays;
         final boolean serverStats;
@@ -36,22 +40,21 @@ public class SongsCommand extends Command {
         if (args.length >= 1) {
             String day = args[args.length == 1 ? 0 : 1];
             lastDays = getLastDays(day);
-            serverStats = args[0].equalsIgnoreCase("server") || (args.length == 1 && day.matches(REGEX));
+            serverStats = !MENTION_PATTERN.matcher(args[0]).matches();
         } else {
             serverStats = true;
             lastDays = Math.min(DEFAULT_DAYS, MAX_FETCH_DAYS);
         }
 
+        final EmbedBuilder embedBuilder = getEmbed(message.getGuild(), message.getAuthor(), lastDays, serverStats);
         addListToEmbed(embedBuilder, getBot().getDatabase().getTrackDao().getTopTracks(
-                message.getGuild().getId(), serverStats ? null : member.getId(), 10, lastDays),
-                lastDays);
+                message.getGuild().getId(), serverStats ? null : member.getId(), 10, lastDays));
 
         message.getChannel().sendMessage(embedBuilder.build()).queue();
     }
 
-    private void addListToEmbed(final EmbedBuilder embedBuilder, final Map<SpotifyTrack, Integer> topMap,
-            final int days) {
-        embedBuilder.setTitle("Top 10 Spotify Tracks over %s day%s".formatted(days, days == 1 ? "" : "s"));
+    private void addListToEmbed(final EmbedBuilder embedBuilder, final Map<SpotifyTrack, Integer> topMap) {
+        embedBuilder.setTitle("Top 10 Spotify Tracks");
         if (!topMap.isEmpty()) {
             embedBuilder.setThumbnail(topMap.keySet().iterator().next().albumImageUrl());
         }
@@ -73,4 +76,10 @@ public class SongsCommand extends Command {
         return Math.min(days, MAX_FETCH_DAYS);
     }
 
+    private EmbedBuilder getEmbed(final Guild guild, final User requester, final int days, final boolean serverStats) {
+        return new EmbedBuilder().setFooter("@%s#%s | %d day%s | %s"
+                        .formatted(requester.getName(), requester.getDiscriminator(),
+                                days, days == 1 ? "" : "s", serverStats ? "Server" : "User"),
+                requester.getEffectiveAvatarUrl()).setColor(guild.getSelfMember().getColor());
+    }
 }
