ead4bdced574a97d169fe9549d6813df7ee0b2e9
Markus K <m.kording@hotmail.de>
2020-10-02 21:43:18 +0200

    Add day select in TimeCommand

--- src/main/java/de/biosphere/spoticord/database/dao/UserDao.java
+++ src/main/java/de/biosphere/spoticord/database/dao/UserDao.java
@@ -11,7 +11,19 @@ public interface UserDao {
      * @param userId  the Snowflake id of the user
      * @return The time a user listened to music
      */
-    Long getListenTime(final String guildId, final String userId);
+    default Long getListenTime(final String guildId, final String userId) {
+        return getListenTime(guildId, userId, 0);
+    }
+
+    /**
+     * Returns the time a user has listened to music
+     *
+     * @param guildId the Snowflake id of the guild that the user is part of
+     * @param userId  the Snowflake id of the user
+     * @param lastDays the last days for data collection when 0 all data
+     * @return The time a user listened to music
+     */
+    Long getListenTime(final String guildId, final String userId, final Integer lastDays);
 
     Long getListenTime(final String guildId);
 
@@ -52,7 +64,22 @@ public interface UserDao {
      * @param count   the length of the map
      * @return A sorted map with <code>count</code> entrys
      */
-    Map<String, Long> getTopListenersByTime(final String guildId, final Integer count);
+    default Map<String, Long> getTopListenersByTime(final String guildId, final Integer count) {
+        return getTopListenersByTime(guildId, count, 0);
+    }
+
+    /**
+     * Returns a map of the users with the highest listening time on a guild The
+     * <code>count</code> argument specifies the length of the map. Map contains
+     * {@link String} as Snowflake id of a user and {@link Integer} as the amount of
+     * entrys to be returned.
+     *
+     * @param guildId the Snowflake id of the guild
+     * @param count   the length of the map
+     * @param lastDays the last days for data collection when 0 all data
+     * @return A sorted map with <code>count</code> entrys
+     */
+    Map<String, Long> getTopListenersByTime(final String guildId, final Integer count, final Integer lastDays);
 
     /**
      * Deletes all database entries that are linked to a specific user
