62090002d95fc97e19438ac950d7e91ab4b2e5a3
Markus K <m.kording@hotmail.de>
2020-11-20 18:45:08 +0100

    Uodate DayParser

--- src/main/java/de/biosphere/spoticord/enums/TimeShortcut.java
+++ src/main/java/de/biosphere/spoticord/enums/TimeShortcut.java
@@ -1,6 +1,7 @@
 package de.biosphere.spoticord.enums;
 
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -32,13 +33,36 @@ public enum TimeShortcut {
     }
 
     public static TimeShortcut getShortcut(final Character value) {
+        return getShortcut(value, DAY);
+    }
+
+    public static TimeShortcut getShortcut(final Character value, final TimeShortcut defaultValue) {
         for (final TimeShortcut timeShortcut : values()) {
             if (timeShortcut.getCharacters() == null) continue;
             for (final Character character : timeShortcut.getCharacters()) {
-                if (character == value) return timeShortcut;
+                if (Character.toLowerCase(value) == Character.toLowerCase(character)) return timeShortcut;
             }
         }
-        return DAY;
+        return null;
+    }
+
+    public static List<Character> getShortcuts() {
+        final List<Character> characters = new ArrayList<>();
+        for (final TimeShortcut value : values()) {
+            characters.addAll(value.getCharacters());
+        }
+        return characters;
+    }
+
+    /**
+     * @return a string with all shortcuts in a string. Like dwmy
+     */
+    public static String getAsString() {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (final Character shortcut : getShortcuts()) {
+            stringBuilder.append(shortcut);
+        }
+        return stringBuilder.toString();
     }
 
     private static int toDays(final ChronoUnit chronoUnit) {
